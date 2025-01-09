f234fc797a16d884d1ecd8bb83bae6453547953c
Mohan2999 <158986522+Mohan2999@users.noreply.github.com>
2025-01-09 11:52:06 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+// functions:-
+// -funtion is a block of code used to execute specific task assigned to 
+// -function is a non-primitive data set
+// -funtions can be reusable.
+
+// syntax:-      function function_name(optimal parameters){// function 
+
+
+// function with parameters
+// function types:-
+// -named function
+// -anonymus function
+// let x = ()=>{console,log("Arrow function"):
+// x=();
+
+// }
+let  x=_=>{console.log("Hi");
+         console.log("Hello");
+         console.log("Bye");}
+    x();
+    {
+};
+
